ee7fcc3aa9707f2fb03a228a2042d19a810278fe
Landry Norris <landry.norris@live.com>
2022-04-13 14:00:10 -0500

    Added check for whether JVM is Android or other JVM.

--- platforminfo/src/mingwX86Main/kotlin/com/landry/platforminfo/PlatformInfo.kt
+++ platforminfo/src/mingwX86Main/kotlin/com/landry/platforminfo/PlatformInfo.kt
@@ -1,4 +1,4 @@
 package com.landry.platforminfo
 
 internal actual val platform: Platform =  Platform.WINDOWS
-internal actual val arch: Arch = Arch.X86
+internal actual val arch: Arch = Arch.X86_32
